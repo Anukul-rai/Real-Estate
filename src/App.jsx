4802c66c11517eaf006727d056fc0e55a6b15125
anukul rai <anukul.rau@gmail.com>
2025-06-26 22:49:26 +0545

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/HEader'
+import About from './components/About'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className='w-full overflow-hidden'>
     <Header/>
+    <About/>
     </div>
   )
 }
