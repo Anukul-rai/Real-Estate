5e260c37fe03af14d6c9b4bba8a7b1f7e4e3c8d0
anukul rai <anukul.rau@gmail.com>
2025-06-26 20:52:42 +0545

    new project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from './components/HEader'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='w-full overflow-hidden'>
+    <Header/>
+    </div>
+  )
+}
+
+export default App
